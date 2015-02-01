6f1c015c2c462e3d8766a00815b7c2f6cd44f5fb
ljnissen <lj.nissen@gmail.com>
2015-02-01 10:58:58 +0000

    Second commit

--- basics.rb
+++ basics.rb
@@ -11,3 +11,19 @@ end
 get '/form' do
 	erb :form
 end
+
+post '/form' do
+	"You said '#{params[:message]}'"
+end
+
+get '/secret' do
+	erb :secret
+end
+
+post '/secret' do
+	params[:secret].reverse
+end
+
+get '/decrypt/:secret' do
+	params[:secret].reverse
+end
